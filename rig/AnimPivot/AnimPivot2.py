168bab5e503462c11374ab6183e05032d83d4606
timmyliang(梁伟添) <timmyliang@tencent.com>
2019-12-30 14:30:10 +0800

    更新

--- rig/AnimPivot/AnimPivot2.py
+++ rig/AnimPivot/AnimPivot2.py
@@ -9,7 +9,8 @@ __date__ = '2019-12-27 10:54:44'
 解决动态偏移的问题 - 导致无法回到原点_(:з」∠)_
 参考： https://forums.autodesk.com/t5/maya-programming/attributechange-scriptjob-and-its-lying-ways-help/td-p/6217887
 
-后来发现不需要动态更新 scirptjob 就可以了
+~后来发现不需要动态更新 scirptjob 就可以了~
+我错了，还是需要实时更新
 """
 
 import pymel.core as pm
@@ -18,82 +19,119 @@ import maya.api.OpenMaya as om
 from textwrap import dedent
 import uuid
 
-# def pivotEvent(sel,loc,msg, m_plug, otherMplug, clientData):
+def pivotEvent(sel,loc,msg, m_plug, otherMplug, clientData):
 
-#     # msg  kIncomingDirection = 2048
-#     # msg  kAttributeSet = 8
-#     # 2048 + 8 = 2056
-#     if msg == 2056:
-#         node,attr = m_plug.name().split(".")
-#         if str(loc) == node and "translate" in attr and pm.objExists(loc):
-#             pm.xform(sel, piv=pm.xform(loc,q=1,ws=1,t=1), ws=1)
+    # msg  kIncomingDirection = 2048
+    # msg  kAttributeSet = 8
+    # 2048 + 8 = 2056
+    if msg == 2056:
+        if not pm.objExists(sel) or not pm.objExists(loc):
+            return
+        node,attr = m_plug.name().split(".")
+        if str(loc) == node and "translate" in attr and pm.objExists(loc):
+            pm.xform(sel, piv=pm.xform(loc,q=1,ws=1,t=1), ws=1)
 
+def deleteCallback(idx,event):
+    om.MMessage.removeCallback(idx)
+    if pm.scriptJob(q=1,ex=event):
+        pm.scriptJob(k=event,f=1)
 
-# def addAttrCallback(sel,loc):
-#     # Get sphere's MObject
-#     sel_list = om.MSelectionList()
-#     sel_list.add(str(loc))
-#     _,node = sel_list.getComponent(0)
+def addAttrCallback(sel,loc):
 
-#     # Connect callback to event
-#     idx = om.MNodeMessage.addAttributeChangedCallback(node, partial(pivotEvent,sel,loc))
-#     deleteEvent = lambda:om.MMessage.removeCallback(idx)
-#     pm.scriptJob( ro=1,nd= [str(loc),deleteEvent], protected=True)
-#     pm.scriptJob( ro=1,e= ["SceneOpened",deleteEvent], protected=True)
-
-
-# def pivotEvent(sel,loc):
-#     x,y,z = loc.getTranslation(space="world")
-#     pm.move(x,y,z,sel.scalePivot,sel.rotatePivot,ws=1)
-
-def deleteEvent(idx,node):
-    if pm.scriptJob(q=1,ex=idx):
-        pm.scriptJob(k=idx,f=1)
-    if pm.objExists(node):
-        pm.delete(node)
-
-def updatePivot(sel,loc):
-    pos = pm.xform(loc,q=1,ws=1,t=1)
-    pm.xform(sel, piv=pos, ws=1)
-
-def createScriptJob(sel,loc):
     UUID = str(uuid.uuid4()).replace("-","_")
     node = 'pivotFollow_%s' % UUID
+
+    # Get sphere's MObject
+    sel_list = om.MSelectionList()
+    sel_list.add(str(loc))
+    _node = sel_list.getDependNode(0)
+
+    # Connect callback to event
+    idx = om.MNodeMessage.addAttributeChangedCallback(_node, partial(pivotEvent,sel,loc))
+
+    rotateEvent = lambda: sel.r.set(loc.r.get() + sel.r.get())
+    event = pm.scriptJob( attributeChange=['%s.r'%loc,rotateEvent] )
+
+    deleteEvent = partial(deleteCallback,idx,event)
+    pm.scriptJob( ro=1,nd= [str(loc),deleteEvent], protected=True)
+    pm.scriptJob( ro=1,e= ["SceneOpened",deleteEvent], protected=True)
     
-    # idx = pm.scriptJob( attributeChange=['%s.t'%loc,partial(pivotEvent,sel,loc)] )
-    pivotEvent = partial(updatePivot,sel,loc)
-    idx = pm.scriptJob( attributeChange=['%s.t'%loc,pivotEvent] )
-
-    _deleteEvent = partial(deleteEvent,idx,node)
-    pm.scriptJob( ro=1,nd= [str(loc),_deleteEvent], protected=True)
-    pm.scriptJob( ro=1,e= ["SceneOpened",_deleteEvent], protected=True)
-
+    
     script = dedent("""
     import pymel.core as pm
     from functools import partial
 
-    def deleteEvent(idx,node):
-        if pm.scriptJob(q=1,ex=idx):
-            pm.scriptJob(k=idx,f=1)
-        if pm.objExists(node):
-            pm.delete(node)
+    def pivotEvent(sel,loc,msg, m_plug, otherMplug, clientData):
+        if msg == 2056:
+            if not pm.objExists(sel) or not pm.objExists(loc):
+                return
+            node,attr = m_plug.name().split(".")
+            if str(loc) == node and "translate" in attr and pm.objExists(loc):
+                pm.xform(sel, piv=pm.xform(loc,q=1,ws=1,t=1), ws=1)
 
-    def updatePivot():
-        pos = pm.xform("{1}",q=1,ws=1,t=1)
-        pm.xform("{0}", piv=pos, ws=1)
-
-    if pm.objExists("{0}") and pm.objExists("{1}"):
-        print "load"
+    if pm.objExists("{sel}") and pm.objExists("{loc}"):
+        sel_list = om.MSelectionList()
+        sel_list.add("{loc}")
+        _node = sel_list.getDependNode(0)
+        idx = om.MNodeMessage.addAttributeChangedCallback(_node, partial(pivotEvent,"{sel}","{loc}"))
+        deleteEvent = lambda: om.MMessage.removeCallback(idx)
+        pm.scriptJob( ro=1,nd= ["{loc}",deleteEvent], protected=True)
+        pm.scriptJob( ro=1,e= ["SceneOpened",deleteEvent], protected=True)
         
-        idx = pm.scriptJob( attributeChange=['{1}.t',updatePivot] )
-        _deleteEvent = partial(deleteEvent,idx,"{2}")
-        pm.scriptJob( ro=1,nd= ["{1}",_deleteEvent], protected=True)
-        pm.scriptJob( ro=1,e= ["SceneOpened",_deleteEvent], protected=True)
-        
-    elif pm.objExists("{2}"):
-        pm.delete("{2}")
-    """.format(sel,loc,node))
+    elif pm.objExists("{node}"):
+        pm.delete("{node}")
+    """.format(sel=sel,loc=loc,node=node))
     pm.scriptNode( st=1, bs=script, n=node, stp='python')
+
+
+# def deleteEvent(idx,node):
+#     if pm.scriptJob(q=1,ex=idx):
+#         pm.scriptJob(k=idx,f=1)
+#     if pm.objExists(node):
+#         pm.delete(node)
+
+# def updatePivot(sel,loc):
+#     pos = pm.xform(loc,q=1,ws=1,t=1)
+#     pm.xform(sel, piv=pos, ws=1)
+
+# def createScriptJob(sel,loc):
+#     UUID = str(uuid.uuid4()).replace("-","_")
+#     node = 'pivotFollow_%s' % UUID
+    
+#     # idx = pm.scriptJob( attributeChange=['%s.t'%loc,partial(pivotEvent,sel,loc)] )
+#     pivotEvent = partial(updatePivot,sel,loc)
+#     idx = pm.scriptJob( attributeChange=['%s.t'%loc,pivotEvent] )
+
+#     _deleteEvent = partial(deleteEvent,idx,node)
+#     pm.scriptJob( ro=1,nd= [str(loc),_deleteEvent], protected=True)
+#     pm.scriptJob( ro=1,e= ["SceneOpened",_deleteEvent], protected=True)
+
+#     script = dedent("""
+#     import pymel.core as pm
+#     from functools import partial
+
+#     def deleteEvent(idx,node):
+#         if pm.scriptJob(q=1,ex=idx):
+#             pm.scriptJob(k=idx,f=1)
+#         if pm.objExists(node):
+#             pm.delete(node)
+
+#     def updatePivot():
+#         pos = pm.xform("{1}",q=1,ws=1,t=1)
+#         pm.xform("{0}", piv=pos, ws=1)
+
+#     if pm.objExists("{0}") and pm.objExists("{1}"):
+#         print "load"
+        
+#         idx = pm.scriptJob( attributeChange=['{1}.t',updatePivot] )
+#         _deleteEvent = partial(deleteEvent,idx,"{2}")
+#         pm.scriptJob( ro=1,nd= ["{1}",_deleteEvent], protected=True)
+#         pm.scriptJob( ro=1,e= ["SceneOpened",_deleteEvent], protected=True)
+        
+#     elif pm.objExists("{2}"):
+#         pm.delete("{2}")
+#     """.format(sel,loc,node))
+#     pm.scriptNode( st=1, bs=script, n=node, stp='python')
 
     
 
@@ -126,16 +164,16 @@ def generateAnimPivotLocator():
             loc_shape.localScaleZ.set(40)
 
             # NOTE 锁定并隐藏无关的属性
-            pm.setAttr(loc.rx,l=1,k=0,cb=0)  
-            pm.setAttr(loc.ry,l=1,k=0,cb=0)  
-            pm.setAttr(loc.rz,l=1,k=0,cb=0)  
-            pm.setAttr(loc.sx,l=1,k=0,cb=0)  
-            pm.setAttr(loc.sy,l=1,k=0,cb=0)  
-            pm.setAttr(loc.sz,l=1,k=0,cb=0)  
+            # pm.setAttr(loc.rx,l=1,k=0,cb=0)
+            # pm.setAttr(loc.ry,l=1,k=0,cb=0)
+            # pm.setAttr(loc.rz,l=1,k=0,cb=0)
+            # pm.setAttr(loc.sx,l=1,k=0,cb=0)  
+            # pm.setAttr(loc.sy,l=1,k=0,cb=0)  
+            # pm.setAttr(loc.sz,l=1,k=0,cb=0)  
             # loc.t.connect(sel.rotatePivot)    
 
-            createScriptJob(sel,loc)
-            # addAttrCallback(sel,loc)
+            # createScriptJob(sel,loc)
+            addAttrCallback(sel,loc)
 
             loc_list.append(loc)
 
