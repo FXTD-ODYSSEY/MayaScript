641750c0435f8c10238edff1140552e35621c8b1
timmyliang(梁伟添) <timmyliang@tencent.com>
2020-03-27 16:09:45 +0800

    完善 progress Context 的编写

--- reference/progressContext.py
+++ reference/progressContext.py
@@ -8,33 +8,24 @@ __date__ = '2020-03-26 12:41:54'
 https://stackoverflow.com/questions/29708445/how-do-i-make-a-contextmanager-with-a-loop-inside
 """
 
-from contextlib import contextmanager
 from maya import cmds
 import traceback
 
 def loop(seq):
+    cmds.progressWindow(	
+        title="test" ,
+        progress=0.0,
+        isInterruptable=True )
     total = len(seq)
-    for i in seq:
+    for i,item in enumerate(seq):
         try:
             if cmds.progressWindow( query=True, isCancelled=True ) : break
             cmds.progressWindow( e=True, progress=float(i)/total*100 )
-            yield i  # with body executes here
+            yield item  # with body executes here
         except:
             traceback.print_exc()
-
-@contextmanager
-def progressWindow(title="",status="",data=[]) :
-    cmds.progressWindow(	
-        title=title ,
-        progress=0.0,
-        isInterruptable=True )
-    try :
-        yield loop(data)
-    except:
-        traceback.print_exc()
-    finally :
-        cmds.progressWindow(ep=1)
+            cmds.progressWindow(ep=1)
+    cmds.progressWindow(ep=1)
    
-with progressWindow(data=range(5000)) as data:
-    for i in data:
-        print i
+for i in loop(range(5000)):
+    print i
